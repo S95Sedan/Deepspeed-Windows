23097fe181a32dced14d9c89335db0bee84eac1d
S95Sedan <89262821+S95Sedan@users.noreply.github.com>
2024-02-21 15:12:18 +0100

    Added 13.2

---
+++ modifications/13.2/build_win.bat
@@ -0,0 +1,24 @@
+@echo off
+
+set DS_BUILD_AIO=0
+set DS_BUILD_SPARSE_ATTN=0
+set DS_BUILD_EVOFORMER_ATTN=0
+
+set DS_BUILD_CUTLASS_OPS=0
+set DS_BUILD_RAGGED_DEVICE_OPS=0
+set DS_BUILD_INFERENCE_CORE_OPS=0
+
+echo Administrative permissions required. Detecting permissions...
+
+net session >nul 2>&1
+if %errorLevel% == 0 (
+    echo Success: Administrative permissions confirmed.
+) else (
+    echo Failure: Current permissions inadequate.
+    goto end
+)
+
+
+python setup.py bdist_wheel
+
+:end
